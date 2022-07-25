8231994a835c2ccff018f32731ffd67aa9f2fe2d
carolina castellanos <carolina18_castellanos@hotmail.com>
2022-07-25 12:45:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const URL = 'https://jsonplaceholder.typicode.com/users';
+const cuerpoTabla = document.querySelector('#cuerpoTabla');
+fetch (URL)
+.then((res) => res.json())
+.then((users) => {
+users.forEach(user => {
+    const { id, name, username, email} = user
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <td>${id}</td>
+    <td>${name}</td>
+    <td>${username}</td>
+    <td>${email}</td>
+    `;
+    cuerpoTabla.append(row);
+    console.log(id, name, username, email)
+});
+})
